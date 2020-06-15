d88fa3b29a3a003f341231d4e8feedb6429d4259
Sunil Adari <asunilsingh2205@gmail.com>
2020-06-15 16:28:22 +0530

    Added get profile from the auth0 server

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import Home from "./Home";
 import Profile from "./Profile";
 import Nav from "./Nav";
@@ -22,7 +22,16 @@ export default class App extends Component {
 						exact
 						render={(props) => <Home auth={this.auth} {...props} />}
 					/>
-					<Route path="/profile" component={Profile} />
+					<Route
+						path="/profile"
+						render={(props) =>
+							this.auth.isAuthenticated() ? (
+								<Profile auth={this.auth} {...props} />
+							) : (
+								<Redirect to="/" />
+							)
+						}
+					/>
 					<Route
 						path="/callback"
 						render={(props) => (
